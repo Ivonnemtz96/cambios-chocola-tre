29fdb50946433a194ba237a48db0fa17ba77d170
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-01 15:53:26 -0700

    Se cambia formulario a la parte de abajo, se agrega horario, img de redes sociales

--- modules/index.php
+++ modules/index.php
@@ -136,21 +136,23 @@
             }
         ?>
         <div class="productos">
-            <img style=" height: 400px;" class="product" src="<?php echo $value['imagen']; ?>"
-                alt="image not found" />
+            <img style=" height: 400px;" class="product" src="<?php echo $value['imagen']; ?>" alt="image not found" />
 
             <!-- <a href="detail.php?cat=<?php echo urlencode($categoria); ?>&prod=<?php echo $value['id']; ?>"
                 title="Ver producto">Ver producto</a> -->
 
-                <h3>
-                    <a style="color:#3d180b !important;"  href="detail.php?cat=<?php echo urlencode($categoria); ?>&prod=<?php echo $value['id']; ?>"><?php echo $value['nombre']; ?></a>
-                </h3>
-                <div style="max-height: 8rem; overflow: hidden; margin-bottom: 1rem;">
-                    <p style="color:#3d180b;" >
-                        <?php echo $value['precio']; ?> </br>
-                    </p>
-                    <h4 ><a style="color:#3d180b !important;"  href="detail.php?cat=<?php echo urlencode($categoria); ?>&prod=<?php echo $value['id']; ?>"  >Ver más</a></h4>
-                </div>
+            <h3>
+                <a style="color:#3d180b !important;"
+                    href="detail.php?cat=<?php echo urlencode($categoria); ?>&prod=<?php echo $value['id']; ?>"><?php echo $value['nombre']; ?></a>
+            </h3>
+            <div style="max-height: 8rem; overflow: hidden; margin-bottom: 1rem;">
+                <p style="color:#3d180b;">
+                    <?php echo $value['precio']; ?> </br>
+                </p>
+                <h4><a style="color:#3d180b !important;"
+                        href="detail.php?cat=<?php echo urlencode($categoria); ?>&prod=<?php echo $value['id']; ?>">Ver
+                        más</a></h4>
+            </div>
         </div>
 
         <?
@@ -377,7 +379,7 @@
     </section>
 
     <!-- Order Section -->
-    <div class="order-section container-fluid no-padding">
+    <div hidden class="order-section container-fluid no-padding">
         <div class="section-padding"></div>
         <div class="container">
             <div class="row">
@@ -437,71 +439,99 @@
     <!-- Order Section /- -->
 
     <!-- Contact Us 1 -->
-    <div class="contact-us-1 container-fluid no-paddin  g">
+    <div class="contact-us-1 container-fluid no-padding">
         <div class="section-padding"></div>
         <!-- Container -->
         <div class="container">
-            <div class="col-md-6 col-sm-6 col-xs-6">
-                <!-- Section Header -->
-                <div class="section-header left-header">
-                    <h3>Contáctanos</h3>
-                    <h5>Chocola-Tre Cacao Mexicano</h5>
-                    <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3">
-                    <p>
-                        ¡Hola! Déjanos un mensaje con cualquier duda, sugerencia, cotización que necesites,
-                        estamos para servirte.
-                    </p>
-                </div>
-                <!-- Section Header /- -->
-                <form class="row">
-                    <div class="form-group col-md-12">
-                        <input type="text" id="nombre" name="nombre" required="" placeholder="Nombre"
-                            class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <input type="text" id="email" name="email" required="" placeholder="Correo"
-                            class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <input type="text" id="tel" required="" name="tel" placeholder="Teléfono" class="form-control">
-                    </div>
-                    <div class="form-group col-md-12">
-                        <textarea id="msj" name="msj" placeholder="Mensaje" rows="14" class="form-control"></textarea>
-                    </div>
-                    <input type="submit" title="Send Now" value="Enviar" id="btn_submit" name="Send Now">
-                    <div id="alert-msg" class="alert-msg"></div>
-                </form>
+            <div class="section-header left-header">
+                <h3>Visítanos</h3>
+                <h5>Chocola-Tre Cacao Mexicano</h5>
+                <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3">
+                <p>
+                    Ven y prueba nuestros productos. ¡Te encantarán!
+                </p>
             </div>
-
-            <div class="col-md-6 col-sm-6 col-xs-6 contact-detail">
-                <div class="col-md-6 col-sm-6 col-xs-6 contact-content">
-                    <div class="contact-info">
-                        <h3>Dirección</h3>
-                        <p>Blvd. Antonio Mijares(junto a Banamex)</p>
-                        <p>Col. Centro</p>
-                        <p>San José del Cabo, B.C.S</p>
-                    </div>
+            <div class="row justify-content-center">
+                <div class="col-4 col-md-4">
+                    <a href="">
+                        <img src="images/fb.png" alt="chocolatre">
+                    </a>
                 </div>
-                <div class="col-md-6 col-sm-6 col-xs-6 contact-content">
-                    <div class="contact-info">
-                        <h3>Contacto</h3>
-                        <p><a href="tel:+526241526288" title="Teléfono fijo">(624-152-6288)</a> -- Teléfono fijo</p>
-                        <p><a href="tel:+526241136699" title="Ventas">(624-113-6699)</a> -- Ventas</p>
+                <div class="col-4 col-md-4">
+                    <a href="">
+                        <img src="images/insta.png" alt="chocolatre">
+                    </a>
+                </div>
+                <div class="col-md-12 col-sm-12 col-xs-12 contact-detail" style="padding-top: 10px;">
+                    <div class="col-md-4 col-sm-4 col-xs-4 contact-content">
+                        <div class="contact-info">
+                            <h3>Dirección</h3>
+                            <p>Blvd. Antonio Mijares(junto a Banamex)</p>
+                            <p>Col. Centro</p>
+                            <p>San José del Cabo, B.C.S</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4 contact-content">
+                        <div class="contact-info">
+                            <h3>Contacto</h3>
+                            <p><a href="tel:+526241526288" title="Teléfono fijo">(624-152-6288)</a> -- Teléfono fijo</p>
+                            <p><a href="tel:+526241136699" title="Ventas">(624-113-6699)</a> -- Ventas</p>
+                            <p>
+                                <a href="mailto:info@chocola-tre.com" title="info@chocola-tre.com">
+                                    info@chocola-tre.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4 contact-content">
+                        <div class="contact-info">
+                            <h3>Horario</h3>
+                            <p>Lunes - Sábado: 09:00am a 20:00pm</p>
+                            <p>Domingo: Cerrado</p>
+
+                        </div>
+                    </div>
+                    <!-- Map Section -->
+                    <div class="mapa">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.033117080809!2d-109.69586178590691!3d23.059247420744764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5b5e2c583ca5%3A0x9999bd37cb735672!2sChocolaTre!5e0!3m2!1sen!2smx!4v1656954088879!5m2!1sen!2smx"
+                            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <!--  Map Section /- -->
+                </div>
+                <div class="col-md-10 col-sm-10 col-xs-10" style="margin-top: 3rem;">
+                    <div class="section-header left-header">
+                        <h3>Escríbenos</h3>
+                        <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3">
                         <p>
-                            <a href="mailto:info@chocola-tre.com" title="info@chocola-tre.com">
-                                info@chocola-tre.com</a>
+                            ¿Tienes alguna duda o pregunta? <br> Estamos para servirte.
                         </p>
                     </div>
+                    <form class="row">
+                        <div class="form-group col-md-12">
+                            <input type="text" id="nombre" name="nombre" required="" placeholder="Nombre"
+                                class="form-control">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="text" id="email" name="email" required="" placeholder="Correo"
+                                class="form-control">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="text" id="tel" required="" name="tel" placeholder="Teléfono"
+                                class="form-control">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <textarea id="msj" name="msj" placeholder="Mensaje" rows="14"
+                                class="form-control"></textarea>
+                        </div>
+                        <input type="submit" name="submit" title="Send Now" value="Enviar" id="btn_submit"
+                            name="Send Now">
+                        <div id="alert-msg" class="alert-msg"></div>
+                    </form>
                 </div>
-                <!-- Map Section -->
-                <div class="mapa">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.033117080809!2d-109.69586178590691!3d23.059247420744764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5b5e2c583ca5%3A0x9999bd37cb735672!2sChocolaTre!5e0!3m2!1sen!2smx!4v1656954088879!5m2!1sen!2smx"
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-                <!--  Map Section /- -->
             </div>
+
+
         </div><!-- Container /- -->
         <div class="section-padding"></div>
     </div>
